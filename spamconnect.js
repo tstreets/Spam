3d5aec8f1a58f1f33753da0a790d73c1435df47f
Michael Champlin <themikesshow@gmail.com>
2024-11-26 11:14:50 -0500

    rest of songs

---
+++ spamconnect.js
@@ -0,0 +1,10 @@
+import { app } from "./firebase.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
