185c83459f96ea0bdf5af11896c731f6573fdc9c
Michael Champlin <themikesshow@gmail.com>
2024-12-12 10:01:56 -0500

    finalized

--- spamconnect.js
+++ spamconnect.js
@@ -18,15 +18,15 @@ async function getArtists() {
       dogsRef.innerHTML += `<a href="${artistInfo.url}">${artistInfo.name}</a>
       <div class="artistsect">
         <img src="${artistInfo.cover_image}" alt="Queens" /> <div class="arti">
-          <h3>Little Sister</h3>
+          <h3>${artistInfo.songTitles[0]}</h3>
          <audio src="${artistInfo.songs[0]}" controls="true"></audio>
         </div>
         <div class="arti2">
-          <h3>My God Is The Sun</h3>
+          <h3>${artistInfo.songTitles[1]}</h3>
           <audio src="${artistInfo.songs[1]}" controls="true"></audio>
         </div>
         <div class="arti2">
-          <h3>I Sat By The Ocean</h3>
+          <h3>${artistInfo.songTitles[2]}</h3>
           <audio src="${artistInfo.songs[2]}" controls="true"></audio>
         </div>
         <div class="albumz">
